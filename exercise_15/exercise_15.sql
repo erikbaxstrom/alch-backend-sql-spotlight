21def2ee0816ed462ffbf63d13e8227a01faf409
Erik M Baxstrom <baxstrem@gmail.com>
2022-11-09 16:24:01 -0800

    exercise 15

--- exercise_15/exercise_15.sql
+++ exercise_15/exercise_15.sql
@@ -1,1 +1,7 @@
+-- exercise 15
 -- the total run time of all inventory items if they were all played back to back
+-- expecting 526491
+select sum(film.length)
+from inventory
+inner join film on film.film_id = inventory.film_id;
+
